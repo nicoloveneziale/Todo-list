c096705c77519e3cf54235ba0971f77a4e64ecae
Nicolo Veneziale <nicoloveneziale@gmail.com>
2024-10-16 15:53:33 +0100

    Completed the entry screen

--- src/entry.js
+++ src/entry.js
@@ -5,16 +5,19 @@ import avatar4 from "./images/avatar5.png";
 import avatar5 from "./images/avatar6.png";
 
 const avatarList = [avatar1,avatar2,avatar3,avatar4,avatar5];
+let pfpImg = avatar1;
 
 function createProfileBtn(src) {
     let pfpBtn = document.createElement("btn");
     let pfp = document.createElement("img");
+    pfp.id = "pfp";
     pfp.src = src;
     pfp.alt = "pfp_img";
     pfpBtn.appendChild(pfp);
     pfpBtn.addEventListener("click", (e) => {
-        pfpImg = e.src;
-        console.log(pfpImg);
+        pfpImg = e.target.src;     
+        document.querySelector(".selected").classList.remove("selected");
+        e.target.classList.add("selected");
     })
     return pfpBtn;
 }
@@ -30,9 +33,36 @@ const pfpDiv = document.createElement("div");
 pfpDiv.id = "pfp-div";
 
 for (let i = 0; i < avatarList.length; i++){
-    pfpDiv.appendChild(createProfileBtn(avatarList[i]));
+    var selectorBtn = createProfileBtn(avatarList[i])
+    pfpDiv.appendChild(selectorBtn);
+    if(i == 0){
+        selectorBtn.classList.add("selected");
+    }
 }
 
-entryDiv.append(entryHeading, pfpDiv);
+const nameContainer = document.createElement("div");
+nameContainer.id = "name-div";
 
-export default entryDiv;
+const nameLabel = document.createElement("label");
+nameLabel.for = "name";
+nameLabel.innerHTML = "username"
+
+
+const nameInput = document.createElement("input");
+nameInput.id = "name";
+nameInput.placeholder = "your name";
+
+nameContainer.append(nameLabel, nameInput);
+
+const continueBtn = document.createElement("button");
+continueBtn.id = "continue";
+continueBtn.innerHTML = "continue";
+
+entryDiv.append(entryHeading, pfpDiv, nameContainer,continueBtn);
+
+export {
+    entryDiv, 
+    nameInput,
+    continueBtn,
+    pfpImg
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./styles.css";
-import entryDiv from "./entry.js";
+import { entryDiv, nameInput, continueBtn } from "./entry.js";
 
 const body = document.querySelector("body");
+
+continueBtn.addEventListener("click", () => {
+    const username = nameInput.value;
+    body.removeChild(entryDiv);
+})
 
 body.append(entryDiv);
